78185191e8b51e0cb093b1b15218d588aff47a20
anjana <anjanaponnu2004@gmail.com>
2026-03-27 15:31:41 +0530

    second commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const API_URL = "http://localhost:4000"
+// const API_URL = "http://localhost:4000"
+const API_URL=import.meta.env.VITE_BACKEND_URL;
 
 export const userRegister = async(data)=>{
   try{
